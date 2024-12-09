0961ac8f5d577be46475f8282b71c39de0c755d8
ratnsa <ratankumawat@gmail.com>
2024-12-09 18:21:30 +0530

    test commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Counter app</h1>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
